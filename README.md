737c68567d0a0eb20a500ef008ff938832e4df1f
Jillian Rowe <jillian.e.rowe@gmail.com>
2016-02-18 14:56:41 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+HPC::Runner::Scheduler - Blah blah blah
+
+# SYNOPSIS
+
+    use HPC::Runner::Scheduler;
+
+# DESCRIPTION
+
+HPC::Runner::Scheduler is
+
+# AUTHOR
+
+Jillian Rowe &lt;jillian.e.rowe@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2016- Jillian Rowe
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/HPC/Runner/Scheduler.pm
@@ -0,0 +1,39 @@
+package HPC::Runner::Scheduler;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+HPC::Runner::Scheduler - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use HPC::Runner::Scheduler;
+
+=head1 DESCRIPTION
+
+HPC::Runner::Scheduler is
+
+=head1 AUTHOR
+
+Jillian Rowe E<lt>jillian.e.rowe@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2016- Jillian Rowe
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
